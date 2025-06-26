89b97ba8099c973838c5848f7e7308aeacc65ca9
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-06-26 04:04:35 -0500

    Actualizado modelonew

--- src/main/java/pe/edu/upc/modelos/controller/Modelcontroller.java
+++ src/main/java/pe/edu/upc/modelos/controller/Modelcontroller.java
@@ -1,6 +1,5 @@
 package pe.edu.upc.modelos.controller;
 
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -8,9 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,42 +20,30 @@ public class Modelcontroller {
 
     @GetMapping("/{type}")
     public ResponseEntity<Resource> getModelByType(@PathVariable String type) throws IOException {
-        String folderPath = "models/";
-        String extension;
-
+        String filename;
         if (type.equals("tflite")) {
-            extension = ".tflite";
+            filename = "models/asl_modelov2_pruebav2.tflite";
         } else if (type.equals("json")) {
-            extension = ".json";
+            filename = "models/asl_labels_modelov2_pruebav2.json";
         } else {
-            return ResponseEntity.badRequest().body(null);
+            return ResponseEntity.badRequest().build();
         }
 
-        // Buscar archivo dentro de la carpeta resources/models/
-        ClassPathResource folderResource = new ClassPathResource(folderPath);
-        File folder = folderResource.getFile();
-
-        File[] matchingFiles = folder.listFiles((dir, name) -> name.endsWith(extension));
-
-        if (matchingFiles == null || matchingFiles.length == 0) {
+        ClassPathResource resource = new ClassPathResource(filename);
+        if (!resource.exists()) {
             return ResponseEntity.notFound().build();
         }
 
-        File selectedFile = matchingFiles[0]; // puedes agregar lógica para elegir el más reciente si hay varios
-
-        InputStream inputStream = new FileInputStream(selectedFile);
-        InputStreamResource resource = new InputStreamResource(inputStream);
-
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + selectedFile.getName() + "\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
-
     @GetMapping("/mensaje")
     public Map<String, String> mensaje() {
         Map<String, String> response = new HashMap<>();
         response.put("mensaje", "Hola desde el backend");
         return response;
     }
+
 }

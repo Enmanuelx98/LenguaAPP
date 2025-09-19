635e452984da2842e18cd6c17ad0737eef9b42ea
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-09-19 02:29:48 -0500

    Primer commit v4

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password, enabled) VALUES ('admin', '$2a$12$HiVZzilA7cp05vflCtRrOOdC.tno3RPARhLDKJm5xn5ie2xF9qeEa', true);
+INSERT INTO roles (rol, user_id) VALUES ('ADMIN', 1);
